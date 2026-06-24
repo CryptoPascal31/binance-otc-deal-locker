d9adb8f213b1e2b4af9bffdcc9c1bc34b6e704cb
CryptoPascal31 <CryptoPascal31@users.noreply.github.com>
2026-06-24 13:24:31 +0200

    Clean blacklist

--- frontend/otc-lock/src/lib/blacklist.ts
+++ frontend/otc-lock/src/lib/blacklist.ts
@@ -1,5 +1,5 @@
 import {hash} from '@kadena/cryptography-utils'
 
-const BLACKLIST = ["-c0TGdp5FNrrb_v8Xqlz5k5S7IacdStPJIa446uJosA"]
+const BLACKLIST = ["PLACEHOLDER"]
 
 export const is_blacklisted = (x:string) => BLACKLIST.includes(hash(x + "BL"))
